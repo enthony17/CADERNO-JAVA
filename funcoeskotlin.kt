23a0747871db2da5467dccacc061c5a50593d9be
enthony17 <125404126+enthony17@users.noreply.github.com>
2023-06-06 17:57:05 -0300

    Create funcoeskotlin.kt

---
+++ funcoeskotlin.kt
@@ -0,0 +1,11 @@
+//cria uma funçao (metodo)que escreve na tela
+fun minhaFunção(){
+  println("minha função escreve na tela")
+}
+
+
+fun main(args: Array<String>) {
+  minhaFunção()
+    minhaFunção()
+
+}
