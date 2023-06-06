3a1091f9dd8fdc1033912a03caa709386a55db58
enthony17 <125404126+enthony17@users.noreply.github.com>
2023-06-06 17:42:58 -0300

    Create Variaveisbooleana.kt

---
+++ Variaveisbooleana.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+  //criar uma variavel do tipo booleana (false true)
+  var nomeVariavel: Boolean = false
+  var x: Boolean =false
+
+  println(nomeVariavel)
+  println(x)
+
+ //IF FAZ TESTE E RETORNA BOOLEAN
+  if (x){
+    print("verdadeiro")
+  }
+else{
+  println("falso")
+}
+}
