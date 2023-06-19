198d8c214233e933536a2b67f948fae03f137933
enthony17 <125404126+enthony17@users.noreply.github.com>
2023-06-19 15:56:03 -0300

    Update equacao.kt

--- equacao.kt
+++ equacao.kt
@@ -1,23 +1,39 @@
-fun main(args: Array<String>) {
-  println("Vamos calcular essa equaçao!")
-  println("Digite os numeros para formar sau propria equaçao:")
-  println("Valor de A")
-  var num1 = readLine()!!
-  var a = num1.toDouble()
-  println("Valor de B")
-  var num2 = readLine()!!
-  var b = num2.toDouble()
-  println("Valor de C")
-  var num3 = readLine()!!
-  var c = num3.toDouble()
+//importar a biblioteca matemático para fazer raiz
+import kotlin.math.*
+//criar a função principal main
+fun main() {
+//pedir para o usúario digitar os numeros
+println("Calculo das raizes de uma equação do segundo grau")
+println("ax²+bx+c=0")
+println("Digite o valor de a: ")
+var num1=readLine()!!
+println("Digite o valor b")
+var num2=readLine()!!
+println("Digite o valor c")
+var num3=readLine()!!
+println("Você digitou a: " +num1+",b:"+num2+",c:" +num3) 
+//os numeros digitados são do tipo String, transformar em Double
+var a=num1.toDouble()
+var b=num2.toDouble()
+var c=num3.toDouble()
+
+println("Número do tipo Double-> a:" +a+",b:"+b+",c:"+c)
+//calculo do delta
+var delta=b*b-(4*a*c)
+  println("O delta é:"+delta)
+var raizdelta=sqrt(delta)
+println("A raiz de delta é:"+raizdelta)
+  //calculo x1
+var x1=(-b+raizdelta)/(2*a)
+println("O resultado do x1 é:"+x1)
   
-  
- // var raiz1=((-b+(sqrt((b^2)-(4*a*c))))/2*a)
-  var delta = (b * b) - (4 * a * c)
-  var r1= ((0-b) + (Math.sqrt(delta))) / (2 * a)
-  
-  //var raiz2=(-b-(sqrt(b^2)-(4*a*c)))/2*a
-   var r2= ((0-b) - (Math.sqrt(delta))) / (2 * a)
-   println("O resultado do x1 é: " +r1)
-  println("O resultado do x2 é: " +r2)
+  //calculo x2
+var x2=(-b-sqrt(delta))/(2*a)
+println("O resultado do x2 é:"+x2)
+
+
+
+
+
+
 }
